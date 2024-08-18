c33084de774295d034aae8ebd4a0d5c173c2a456
Philip Deuchler <pbdeuchler@gmail.com>
2024-08-18 00:28:09 -0600

    add eip iam policy

--- iam.tf
+++ iam.tf
@@ -1,3 +1,20 @@
+resource "aws_iam_role" "ec2-postgres" {
+  name = "postgres-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
 resource "aws_iam_policy" "postgres-ebs-bootstrap" {
   name        = "postgres-ebs-boostrap"
   description = "policy to allow postgres ec2 instances to mount ebs volumes"
@@ -24,10 +41,31 @@ resource "aws_iam_policy" "postgres-ebs-bootstrap" {
   })
 }
 
+resource "aws_iam_policy" "postgres-eip-bootstrap" {
+  name        = "postgres-eip-boostrap"
+  description = "policy to allow postgres ec2 instances to associate eip addresses"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "ec2:AssociateAddress",
+      ]
+      Resource = ["*"] 
+      }
+    ]
+  })
+}
 
 resource "aws_iam_role_policy_attachment" "ec2-postgres-ebs-binding" {
   role       = aws_iam_role.ec2-postgres.name
   policy_arn = aws_iam_policy.postgres-ebs-bootstrap.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-postgres-eip-binding" {
+  role       = aws_iam_role.ec2-postgres.name
+  policy_arn = aws_iam_policy.postgres-eip-bootstrap.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ec2-postgres-cloudwatch-binding" {
